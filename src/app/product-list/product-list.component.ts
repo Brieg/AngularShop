2a6faa78d3ef4c77eb23d41e21fde0f640475ccb
Mariusz Briegmann <mbriegmann@sii.pl>
2022-03-30 19:18:09 +0200

    T-007: Product pagination

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 import { PageEvent } from "@angular/material/paginator";
 import { IProduct } from "../interfaces/product";
 import { ProductService } from "../services/product/product.service";
-import { Observable } from "rxjs";
 
 @Component({
   selector: "app-product-list",
   templateUrl: "./product-list.component.html",
-  styleUrls: ["./product-list.component.scss"]
+  styleUrls: ["./product-list.component.scss"],
+  providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
 
   // Pagination
-  length: number = 30;
-  pageSize: number = 3;
+  pageLength: number = 0;
+  pageSize: number = 6;
   pageSizeOptions: number[] = [this.pageSize, this.pageSize * 2];
+  pageEvent: PageEvent | undefined
 
-  // MatPaginator Output
-  pageEvent: PageEvent | undefined;
-  products: Observable<IProduct[]> | null = null;
+  // Products
+  products: IProduct[] = [];
+  paginationProduct: IProduct[] = [];
 
   constructor(
     private productService: ProductService) {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.callApi()
+    this.productService.callApi().subscribe(data => {
+      this.products = data;
+      this.pageLength = data.length;
+      this.paginationProduct = data.slice(((0 + 1) - 1) * this.pageSize).slice(0, this.pageSize);
+    })
+  }
+
+  OnPaginate(event:PageEvent) {
+    const offset = ((event.pageIndex + 1) - 1) * event.pageSize;
+    this.paginationProduct = this.products.slice(offset).slice(0, event.pageSize);
   }
 
 }
