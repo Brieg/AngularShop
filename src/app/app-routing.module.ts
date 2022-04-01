282713e4afa6b71f90b9ba4a7b5e7ab2953898e5
Mariusz Briegmann <mbriegmann@sii.pl>
2022-04-01 11:43:53 +0200

    T-011: Codereview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ProductListComponent } from "./product-list/product-list.component";
-import { ProductPageComponent } from "./product-page/product-page.component";
-
-// const routes: Routes = [
-//   { path: "", component: ProductListComponent },
-//   { path: "product/:productID", component: ProductPageComponent }
-// ];
 
 const routes: Routes = [];
 
